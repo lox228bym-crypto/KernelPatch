177153116e577e8b25e6b08d561d84543afdc246
lox228bym-crypto <lox228bym@gmail.com>
2025-12-28 13:30:14 +0500

    hello.c

--- kpms/demo-hello/hello.c
+++ kpms/demo-hello/hello.c
@@ -1,17 +1,16 @@
 #include <linux/kernel.h>
 #include "../../kernel/patch/include/kpatch.h"
 
-static int hook_sig(void *info, int flags) {
+static int my_hook(void *info, int flags) {
     return 0;
 }
 
 int kpm_init(void) {
     unsigned long addr = (unsigned long)kallsyms_lookup_name("module_sig_check");
     if (addr) {
-        return kpatch_add_hook((void *)addr, (void *)hook_sig, NULL);
+        return kpatch_add_hook((void *)addr, (void *)my_hook, NULL);
     }
     return -1;
 }
 
-void kpm_exit(void) {
-}
+void kpm_exit(void) {}
