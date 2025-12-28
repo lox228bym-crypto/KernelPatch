071f5932f34c313c18011d8f694f0eb5a0f27aa3
lox228bym-crypto <lox228bym@gmail.com>
2025-12-28 13:20:08 +0500

    hello.c

--- kpms/demo-hello/hello.c
+++ kpms/demo-hello/hello.c
@@ -1,17 +1,18 @@
 #include <linux/kernel.h>
 #include <kpatch.h>
 
-// Функция-заглушка: всегда возвращает 0 (успешная проверка подписи)
-int bypass_sig(void *info, int flags) { return 0; }
-
-int kpm_init(void) {
-    // Ищем адрес функции проверки подписи в ядре
-    unsigned long addr = kallsyms_lookup_name("module_sig_check");
-    if (addr) {
-        // Устанавливаем перехват (inline-hook)
-        return kpatch_add_hook(addr, (void *)bypass_sig, NULL);
-    }
-    return -1; // Если символ не найден
+static int hook_sig(void *info, int flags) {
+    return 0;
 }
 
-void kpm_exit(void) {}
+int kpm_init(void) {
+    unsigned long addr = (unsigned long)kallsyms_lookup_name("module_sig_check");
+    if (addr) {
+        return kpatch_add_hook((void *)addr, (void *)hook_sig, NULL);
+    }
+    return -1;
+}
+
+void kpm_exit(void) {
+}
+
