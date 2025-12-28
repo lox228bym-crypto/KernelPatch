d857d3a6a419dbac726578396805468a96b7bf48
lox228bym-crypto <lox228bym@gmail.com>
2025-12-28 14:02:32 +0500

    hello.c

--- kpms/demo-hello/hello.c
+++ kpms/demo-hello/hello.c
@@ -1,11 +1,8 @@
-// Никаких #include! Определяем функции вручную
-extern unsigned long kallsyms_lookup_name(const char *name);
+#include "../../kernel/include/kallsyms.h"
 
 int kpm_init(void) {
     unsigned long addr = kallsyms_lookup_name("module_sig_check");
     if (!addr) return -1;
-
-    // Записываем RET 0 прямо в память
     unsigned int *p = (unsigned int *)addr;
     p[0] = 0xd2800000; // mov x0, #0
     p[1] = 0xd65f03c0; // ret
